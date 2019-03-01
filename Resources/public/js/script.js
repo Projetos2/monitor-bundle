d050c1ba9beb774b563386c1b5aa950c7c3dd04f
Projetos2 <projetos2@unisc.br>
2019-03-01 11:58:33 -0300

    Update script.js

--- Resources/public/js/script.js
+++ Resources/public/js/script.js
@@ -14,7 +14,11 @@
             atendimento: null,
             novoServico: '',
             novaPrioridade: '',
-            connected: false
+            connected: false,
+            totalAtendimentosPendentes: 0,
+            tempoMedioEspera: null,
+            tempoMaximoEspera: null,
+            tempoMinimoEspera: null
         },
         methods: {
             init: function () {
@@ -61,6 +65,75 @@
                     },
                     success: function (response) {
                         self.servicos = response.data;
+
+                        let somaSenhas = 0;
+                        let horas = 0;
+                        let minutos = 0;
+                        let segundos = 0;
+                        let maior = 0;
+                        let maiorHoras = 0;
+                        let maiorMinutos = 0;
+                        let maiorSegundos = 0;
+                        let menor = 9999999999999999999999;
+                        let menorHoras = 0;
+                        let menorMinutos = 0;
+                        let menorSegundos = 0;
+                        
+                        
+                        response.data.forEach(servico=>{
+                        	servico.fila.forEach(atendimento=>{
+                        		somaSenhas += 1;
+
+                        		const hora = atendimento.tempoEspera.split(':')
+                               	const horaDate = new Date(1970, 0, 1, hora[0], hora[1], hora[2])
+                        		
+                        		const timestamp = horaDate.getTime()
+                        		
+  
+                        		if(timestamp > maior){
+                        			maior = timestamp;
+                        			maiorHoras = new Date(maior).getHours();
+                        			maiorMinutos = new Date(maior).getMinutes();
+                        			maiorSegundos = new Date(maior).getSeconds();
+                        		}
+                        		
+                        		if(timestamp < menor){
+                        			menor = timestamp;
+                        			menorHoras = new Date(menor).getHours();
+                        			menorMinutos = new Date(menor).getMinutes();
+                        			menorSegundos = new Date(menor).getSeconds();
+                        		}
+                        		
+                        		horas += horaDate.getHours();
+                        		minutos += horaDate.getMinutes();
+                        		segundos += horaDate.getSeconds(); 
+                        		
+                        		
+                            })
+                        })
+                        
+                        function formataData(n) {
+                        	let k = parseFloat(n).toPrecision(2)
+                        	if(k.length == 3){
+                        		return '0' + k.split('.')[0]
+                        	}else{
+                        		return k
+                        	}
+                        }
+                        
+                        self.tempoMedioEspera = formataData(horas/somaSenhas) 
+                        	+ ':' + formataData(minutos/somaSenhas) 
+                        	+ ':' + formataData(segundos/somaSenhas);
+                        
+                        self.totalAtendimentosPendentes = somaSenhas
+                        
+                        self.tempoMaximoEspera = formataData(maiorHoras) 
+                    	+ ':' + formataData(maiorMinutos) 
+                    	+ ':' + formataData(maiorSegundos);
+                        
+                        self.tempoMinimoEspera = formataData(menorHoras) 
+                    	+ ':' + formataData(menorMinutos) 
+                    	+ ':' + formataData(menorSegundos);
                     }
                 });
             },
